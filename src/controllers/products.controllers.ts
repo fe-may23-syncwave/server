dabd1f1bd826df25cb558cf4da634c7a51300985
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-09-06 15:52:41 +0300

    second fix getting one product

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -40,8 +40,8 @@ export async function getAllProducts(req: Request, res: Response) {
 }
 
 export const getOneProduct = async (req: Request, res: Response) => {
-  const { productId } = req.params;
-  const foundProduct = await getOne(productId);
+  const { itemId } = req.params;
+  const foundProduct = await getOne(itemId);
 
   if (!foundProduct) {
     res.status(404).send('Product not found');
