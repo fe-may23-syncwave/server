45a6da4f09bed1e8cdcb4b8773faa47d581ecfbf
Olena Boldysheva <adv55577@gmail.com>
2023-09-06 11:34:21 +0300

    Update users.router.ts

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { userController } from '../controllers/user.controller';
 import { catchError } from '../middlewares/catchError';
-// import { authMiddleware } from '../middlewares/authMiddleware';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 export const userRouter = express.Router();
 
 userRouter.get(
   '/',
-  // catchError(authMiddleware),
+  catchError(authMiddleware),
   catchError(userController.getAll),
 );
